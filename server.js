55c81681aa91fe1a312d3d2502db1367299f8bc8
Julian Trupe <juliantrupe@gmail.com>
2020-01-05 12:10:57 -0800

    routing

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+require("./app/routing/htmlRoutes")(app);
+
 app.listen(PORT, function () {
     console.log("App listening on PORT: " + PORT);
 })
