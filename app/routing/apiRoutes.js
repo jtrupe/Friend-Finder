8e8d01207479cdc59e094d7e014f59c8b6f57ac5
Julian Trupe <juliantrupe@gmail.com>
2020-01-09 15:49:41 -0800

    some small changes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -12,7 +12,7 @@ module.exports = function (app) {
             photo: '',
             difference: 50
         };
- 
+
         console.log(req.body);
 
         var friendData = req.body;
@@ -20,17 +20,21 @@ module.exports = function (app) {
 
         console.log(friendScores);
 
-        var totalDifference = 0;
+        var totalDifference = -1;
 
         for (var i = 0; i < friends.length; i++) {
             console.log(friends[i]);
-            var totalDifference = 0;
+            var totalDifference = -1;
 
             for (var j = 0; j < friends[i].scores.length; j++) {
                 var diff = Math.abs(friendScores[j] - friends[i].scores[j])
                 totalDifference += diff;
 
-
+                if (j = friends[i].scores.length -1 && totalDifference < bff.difference && totalDifference >= 0) {
+                    bff.name = friends[i].name;
+                    bff.photo = friends[i].photo;
+                    bff.difference = totalDifference;
+                }
             }
         }
     })
